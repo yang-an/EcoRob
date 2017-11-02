39d2a3d256bfe6f0b3a4b7506c277290e1acf6c6
jplattne <jplattne@hm.edu>
2017-11-02 12:38:35 +0100

    added globale defines and variables

--- EcoRob/EcoRob.ino
+++ EcoRob/EcoRob.ino
@@ -1,6 +1,20 @@
 /* 
  *  EcoRob main module
  */
+/***********global defines***********/
+#define DIST_RIGHT    adc[0]
+#define DIST_CENTER   adc[1]
+#define BOOST_U_OUT   adc[2]
+#define BOOST_U_IN    adc[3]
+#define MOTOR_I_RIGHT adc[4]
+#define MOTOR_I_LEFT  adc[5]
+#define DIST_LEFT     adc[6]
+#define ADC_MAX_CH    7
+/***********global defines***********/
+
+/**********global variables**********/
+uint18_t adc[7] = {0};
+/**********global variables**********/
 
 int main() {
   // set pin modes according to specification
