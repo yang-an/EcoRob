4c0e20b7a6b25e698f127af77ce23ed5ccfb5262
Anton Yang <anton.yang@hm.edu>
2017-12-07 10:08:38 +0100

    updated boost controller

--- EcoRob/EcoRob.ino
+++ EcoRob/EcoRob.ino
@@ -18,9 +18,16 @@
 #define DIST_LEFT     adc[6]
 #define AUX_IN        adc[7]
 #define ADC_MAX_CH    8
+//test
+#define KP 200//(DIST_LEFT >> 2)
+#define KI 10//(AUX_IN >> 3)
 
 /**********global variables**********/
 volatile uint16_t adc[8] = {0};
+
+//test
+volatile int32_t y = 0;
+volatile int16_t xd = 0;
 
 int main() {
   /* Init routine:
@@ -33,17 +40,26 @@ int main() {
   Serial.begin(115200);
   init_pins();
   init_adc();
-  set_boost_pwm_ocr(150);
+  //set_boost_pwm_ocr(150);
   init_boost_pwm();
   init_driver_pwm();
   sei();
   
   while (1) {
-    
-    Serial.print(AUX_IN);
-    Serial.print(" ");
-    Serial.println(OCR1B);
-    
+    Serial.print("Kp: ");
+    Serial.print(KP);
+    Serial.print(" | ");
+    Serial.print("Ki: ");
+    Serial.print(KI);
+    Serial.print(" | ");
+    Serial.print("Uout: ");
+    Serial.print(BOOST_U_OUT);
+    Serial.print(" | ");
+    Serial.print("xd: ");
+    Serial.print(xd);
+    Serial.print(" | ");
+    Serial.print("y: ");
+    Serial.println(y);
     _delay_ms(2);
   }
 }
--- EcoRob/boost_pwm.ino
+++ EcoRob/boost_pwm.ino
@@ -7,7 +7,7 @@
  */
 
 #define TC1_TOP                 319   // 16 MHz / 50 kHz - 1 = 319
-#define BOOST_PWM_MAX           303   // 319 * 95% = 303
+#define BOOST_PWM_MAX           287   // 319 * 95% = 303
 #define BOOST_CONTROLLER_GAIN   4    // fixed point 4.4 Bit
 #define BOOST_OUTPUT_REF        785   // 12 V / (1k + 470) * 470 / 5 V * 1023 = 785
 
@@ -30,6 +30,8 @@ void init_boost_pwm() {
 }
 
 inline void set_boost_pwm_ocr(uint16_t ocr) { // inline to avoid a function call
+  // shift right (11.5 fixed point)
+  ocr >>= 5;
   // clamp max value
   OCR1B = ocr <=BOOST_PWM_MAX ? ocr : BOOST_PWM_MAX;
   OCR1C = TC1_TOP - OCR1B;
@@ -47,29 +49,39 @@ void cycle_boost_pwm(uint8_t step_, uint8_t delay_) {
 
 ISR(TIMER0_COMPA_vect)
 {
+  /* output voltage control loop
+  sample frequency: 8 kHz (ADC sample clock)
+  */
   static uint8_t isr_cntr = 0;
-  int16_t xd = 0;
-  static int16_t y = 0;
+  //int16_t xd = 0;
+  static int16_t xdsum = 0;
+  //static int16_t y = 0;
+  
   
   if(isr_cntr < ADC_MAX_CH)
     isr_cntr++;
   else
   {
     isr_cntr = 0;
-    xd = BOOST_OUTPUT_REF - AUX_IN;
-    //xd = BOOST_OUTPUT_REF - BOOST_U_OUT;
-    y = y + ((xd * BOOST_CONTROLLER_GAIN) >> 4);
-    /*
-    Serial.print(xd);
-    Serial.print(" ");
-    Serial.println(y);
-    */
-    if(y > BOOST_PWM_MAX)
-      y = BOOST_PWM_MAX;
-    else if(y < 0)
-      y = 0;
+    //xd = BOOST_OUTPUT_REF - AUX_IN;
+    xd = BOOST_OUTPUT_REF - BOOST_U_OUT;
+    // integrator
+    //y = KP * xd;
 
-    //set_boost_pwm_ocr(150);   // y is already limited -> can be converted to unsigned
+    xdsum += xd;
+    y = (uint32_t)KP * xd + KI * xdsum;
+
+    // shift left (11.5 fixed point)
+    if(y > (BOOST_PWM_MAX << 5)) {
+      y = BOOST_PWM_MAX << 5;
+      xdsum = (BOOST_PWM_MAX << 5);
+    }
+    else if(y < 0) {
+      y = 0;
+      xdsum = 0;
+    }
+    
+    
     set_boost_pwm_ocr((uint16_t)y);   // y is already limited -> can be converted to unsigned
   }
 }
